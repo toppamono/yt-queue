8a88407dd03dff936b5a51160e15f44f5d7eb759
toppamono <tsujino1976@gmail.com>
2026-09-19 14:47:44 +0900

    Bump PWA cache to v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='ytq-shell-v13',S=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const C='ytq-shell-v14',S=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(S)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==self.location.origin)return;e.respondWith(fetch(e.request).then(x=>{const y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
